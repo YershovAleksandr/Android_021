a2575fca821cc1cf822d86487edaf30f9e043950
hp <namstudionsk@gmail.com>
2018-02-04 23:45:10 +0700

    Add all files

--- app/src/main/java/com/nam/myapplication/MainActivity.java
+++ app/src/main/java/com/nam/myapplication/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.widget.Toolbar;
 
 
 
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
